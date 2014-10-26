29bc6f923b89a4bcff6ca202f8e0df848a574f3f
risi21 <luca.parisi.2@alice.it>
2014-10-26 16:48:03 +0100

    Modificato Clone Cluster perchè era cambiato il database del JRSAAS_CONFIG, adesso funziona

--- Sorgenti/Step 6: Jackrabbit Observation/Clone Cluster/build.sh
+++ Sorgenti/Step 6: Jackrabbit Observation/Clone Cluster/build.sh
@@ -2,4 +2,4 @@ mvn clean package
 cd target
 mkdir export
 cp /home/luca/Scaricati/jr2.war ./export/jackrabbit.war
-sudo jar uf CloneCluster-1.0-SNAPSHOT.jar export/jackrabbit.war
+sudo jar uf CloneCluster-1.0-SNAPSHOT.one-jar.jar export/jackrabbit.war
